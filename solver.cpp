7bc7636bd83a4aa3a5a17b5f8ae1da3813e0dbf3
kyonsy <kyonshi297@gmail.com>
2025-07-21 19:52:42 +0900

    横ペアを揃えるのに成功

--- solver.cpp
+++ solver.cpp
@@ -22,12 +22,14 @@ vector<int> ops_n = vector<int>(0);
 void load_problem()
 {
     ifstream input("problem.json");
-    if (!input) {
+    if (!input)
+    {
         cerr << "Error: problem.json not found." << endl;
         exit(1);
     }
     json problem;
-    try {
+    try
+    {
         input >> problem;
         input.close();
 
@@ -38,7 +40,9 @@ void load_problem()
             problem["problem"]["field"]["size"].is_number_integer())
         {
             field_size = problem["problem"]["field"]["size"].get<int>();
-        } else {
+        }
+        else
+        {
             throw std::runtime_error("JSON key 'size' is missing or not an integer.");
         }
 
@@ -46,17 +50,24 @@ void load_problem()
             problem["problem"]["field"]["entities"].is_array())
         {
             field = problem["problem"]["field"]["entities"].get<vector<vector<int>>>();
-        } else {
+        }
+        else
+        {
             throw std::runtime_error("JSON key 'entities' is missing or not a 2D array.");
         }
-
-    } catch (const nlohmann::json::parse_error& e) {
+    }
+    catch (const nlohmann::json::parse_error &e)
+    {
         cerr << "JSON parse error: " << e.what() << endl;
         exit(1);
-    } catch (const nlohmann::json::type_error& e) {
+    }
+    catch (const nlohmann::json::type_error &e)
+    {
         cerr << "JSON type error: " << e.what() << endl;
         exit(1);
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         cerr << "An error occurred: " << e.what() << endl;
         exit(1);
     }
@@ -67,7 +78,7 @@ void load_problem()
     {
         for (int x = 0; x < field_size; x++)
         {
-        int number = field[y][x];
+            int number = field[y][x];
             if (pair_coordinates[number][0] == -1)
             {
                 pair_coordinates[number][0] = x;
@@ -87,13 +98,14 @@ void print_field()
 {
     cout << "手数: " << ops_x.size() << endl;
     cout << "   ";
-    for( int i = 0; i < field_size;i++){
-        cout<< setw(3) << i << "|";
+    for (int i = 0; i < field_size; i++)
+    {
+        cout << setw(3) << i << "|";
     }
     cout << endl;
     for (int y = 0; y < field_size; y++)
     {
-        cout << setw(2) <<y << "|";
+        cout << setw(2) << y << "|";
         for (int x = 0; x < field_size; x++)
         {
             cout << setw(3) << field[y][x] << " ";
@@ -108,12 +120,12 @@ void print_pair_coordinates()
 {
     for (int number = 0; number < max_number; number++)
     {
-        cout << setw(3) << number 
-            << " (" << setw(2) << pair_coordinates[number][0] 
-            << ", " << setw(2) << pair_coordinates[number][1] 
-            << "), (" << setw(2) << pair_coordinates[number][2] 
-            << ", " << setw(2) << pair_coordinates[number][3] 
-            << ")" << endl;
+        cout << setw(3) << number
+             << " (" << setw(2) << pair_coordinates[number][0]
+             << ", " << setw(2) << pair_coordinates[number][1]
+             << "), (" << setw(2) << pair_coordinates[number][2]
+             << ", " << setw(2) << pair_coordinates[number][3]
+             << ")" << endl;
     }
     cout << endl;
 }
@@ -128,9 +140,9 @@ void rotate_field(int x, int y, int n)
 
     for (int number = 0; number < max_number; number++)
     {
-        if (x <= pair_coordinates[number][0] && 
-            pair_coordinates[number][0] < x + n && 
-            y <= pair_coordinates[number][1] && 
+        if (x <= pair_coordinates[number][0] &&
+            pair_coordinates[number][0] < x + n &&
+            y <= pair_coordinates[number][1] &&
             pair_coordinates[number][1] < y + n)
         {
             int dx = pair_coordinates[number][0] - x;
@@ -138,9 +150,9 @@ void rotate_field(int x, int y, int n)
             pair_coordinates[number][0] = x + n - 1 - dy;
             pair_coordinates[number][1] = y + dx;
         }
-        if (x <= pair_coordinates[number][2] && 
-            pair_coordinates[number][2] < x + n && 
-            y <= pair_coordinates[number][3] && 
+        if (x <= pair_coordinates[number][2] &&
+            pair_coordinates[number][2] < x + n &&
+            y <= pair_coordinates[number][3] &&
             pair_coordinates[number][3] < y + n)
         {
             int dx = pair_coordinates[number][2] - x;
@@ -193,7 +205,7 @@ void move_pair1(int target_entity, int goal_x, int goal_y)
     int current_y2 = pair_coordinates[target_entity][3];
 
     int target_pair_x, target_pair_y;
-    int goal_pair_x = goal_x;
+    int goal_pair_x = goal_x + 1;
     int goal_pair_y = goal_y;
 
     // goal_x, goal_y にあるのが1つ目と仮定し、2つ目を動かす
@@ -209,88 +221,140 @@ void move_pair1(int target_entity, int goal_x, int goal_y)
     }
 
     // targetがgoalのすぐ下にある場合
-    if(goal_pair_x == target_pair_x && goal_pair_y + 1 == target_pair_y){
-        rotate_field(goal_pair_x,goal_pair_y,2);
+    if (goal_pair_x - 1 == target_pair_x && goal_pair_y + 1 == target_pair_y)
+    {
+        rotate_field(goal_pair_x - 1, goal_pair_y, 2);
+        return;
+    }
+
+    // targetがgoalの左に達した場合
+    if(goal_pair_x == target_pair_x){
+        if(goal_pair_y == target_pair_y) return;
+        int max_roteta_size;
+        if(field_size - target_pair_x < target_pair_y-goal_pair_y){
+            max_roteta_size = field_size - target_pair_x;
+        }else{
+            max_roteta_size = target_pair_y - goal_pair_y+1;
+        }
+        rotate_field(target_pair_x,target_pair_y - max_roteta_size+1,max_roteta_size);
+    }
+
+    // targetが上に達して、かつgoalの左側にあるとき
+    if (goal_pair_x > target_pair_x && goal_pair_y + 1 == target_pair_y)
+    {
+        int max_rotate_size;
+        if (field_size - target_pair_y < goal_pair_x - target_pair_x)
+        {
+            max_rotate_size = field_size -target_pair_y;
+        }
+        else
+        {
+            max_rotate_size = goal_pair_x - target_pair_x;
+        }
+        rotate_field(target_pair_x, target_pair_y, max_rotate_size);
+        return;
+    }
+
+    // targetが上に達して、かつgoalの右側にあるとき
+    if (goal_pair_x < target_pair_x && goal_pair_y == target_pair_y)
+    {
+        int max_rotate_size;
+        if (field_size - target_pair_y < target_pair_x - goal_pair_x)
+        {
+            max_rotate_size = field_size;
+        }
+        else
+        {
+            max_rotate_size = target_pair_x - goal_pair_x + 1;
+        }
+        rotate_field(target_pair_x -max_rotate_size+1, target_pair_y, max_rotate_size);
+        rotate_field(target_pair_x -max_rotate_size+1, target_pair_y, max_rotate_size);
+        rotate_field(target_pair_x -max_rotate_size+1, target_pair_y, max_rotate_size);
+        return;
     }
 
     // 動かしたいエンティティが目標の右下にある場合
-    if(goal_pair_x < target_pair_x && goal_pair_y < target_pair_y)
+    if (goal_pair_x < target_pair_x && goal_pair_y < target_pair_y)
     {
-        goal_pair_x++;
         int max_rotate_size;
-        if(target_pair_x - goal_pair_x < target_pair_y - goal_pair_y){
+        if (target_pair_x - goal_pair_x < target_pair_y - goal_pair_y)
+        {
             max_rotate_size = target_pair_x - goal_pair_x + 1;
-        }else{
+        }
+        else
+        {
             max_rotate_size = target_pair_y - goal_pair_y + 1;
         }
-        rotate_field(target_pair_x - max_rotate_size + 1, target_pair_y - max_rotate_size + 1,max_rotate_size);
+        rotate_field(target_pair_x - max_rotate_size + 1, target_pair_y - max_rotate_size + 1, max_rotate_size);
         return;
     }
     // 動かしたいエンティティが目標の左下にある場合
-    if(goal_pair_x > target_pair_x && goal_pair_y < target_pair_y){
-        goal_pair_x++;
+    if (goal_pair_x > target_pair_x && goal_pair_y < target_pair_y)
+    {
         int max_rotate_size;
-        if(goal_pair_x - target_pair_x < target_pair_y - goal_pair_y){
-            max_rotate_size = goal_pair_x -target_pair_x + 1;
-        }else{
-            max_rotate_size = goal_pair_y - target_pair_x + 1;
+        if (goal_pair_x - target_pair_x < target_pair_y - goal_pair_y + 1)
+        {
+            max_rotate_size = goal_pair_x - target_pair_x + 1;
         }
-        rotate_field(target_pair_x, target_pair_y - max_rotate_size + 1 , max_rotate_size);
+        else
+        {
+            max_rotate_size = target_pair_y - goal_pair_y;
+        }
+        rotate_field(target_pair_x, target_pair_y - max_rotate_size+1, max_rotate_size);
         return;
     }
-
-    
-
 }
 
 void solve2()
 {
     for (int y = 0; y < field_size; y++) // 下から2行を処理対象外にする
     {
-        for (int x = 0; x < field_size; x+=2) // 右から2列を処理対象外にする
+        for (int x = 0; x < field_size; x += 2) // 右から2列を処理対象外にする
         {
-            if( y == field_size - 2 && x >= field_size - 2) break; // 最後の2×2ブロックは処理しない
+            if (y == field_size - 2 && x >= field_size - 2)
+                break; // 最後の2×2ブロックは処理しない
             int target = field[y][x];
             // (y, x) にある target のペアを (y, x+1) に移動させる
             // ただし、既にペアが揃っている場合はスキップ
-            if (field[y][x+1] == target) continue;
+            if (field[y][x + 1] == target)
+                continue;
 
             move_pair1(target, x, y); // target のペアの片方を (x, y) の隣に移動
-            print_field(); // デバッグ用にフィールドを表示
+            print_field();            // デバッグ用にフィールドを表示
             print_pair_coordinates(); // デバッグ用にペアの座標を表示
         }
     }
 }
 
-
-void solve1(){
-    for (int y = 0; y < field_size; y+=2)
+void solve1()
+{
+    for (int y = 0; y < field_size; y += 2)
     {
-        for (int x = 0; x < field_size; x+=2)
+        for (int x = 0; x < field_size; x += 2)
         {
             int target = field[y][x];
             // ペアが既に揃っていたらスキップ
-            if (target == field[y][x+1]) continue; 
+            if (target == field[y][x + 1])
+                continue;
             int target_pair_x = pair_coordinates[target][2];
             int target_pair_y = pair_coordinates[target][3];
 
-            // X 0 3 3  
-            // 1 1 2 3 
-            // 2 1 2 2 
-            // 2 2 2 3 
+            // X 0 3 3
+            // 1 1 2 3
+            // 2 1 2 2
+            // 2 2 2 3
             // 上は4×4の場合の例。何回でXの右にペアを作れるかを示している
             // 下の二つのif文はは1回でペアを揃えられる時の条件とその導きである
-            if (x == target_pair_x && target_pair_y < field_size - x - 1){
-                rotate_field(x+1, y, target_pair_y +1 );
+            if (x == target_pair_x && target_pair_y < field_size - x - 1)
+            {
+                rotate_field(x + 1, y, target_pair_y + 1);
                 continue;
             }
-            if(x + 1 == target_pair_x && y == target_pair_y){
+            if (x + 1 == target_pair_x && y == target_pair_y)
+            {
                 rotate_field(x, y, 2);
                 continue;
             }
-                
-
-            
         }
     }
 }
@@ -300,14 +364,30 @@ int main()
     load_problem();
 
     print_field();
-    //move_pair1(field[0][0],0,0);
-    //move_pair1(field[0][10],10,0);
-    // for(int x = 0; x < field_size; x += 2){
-    //     while(field[0][x] != field[0][x+1]){
-    //         move_pair1(field[0][x],x,0);
-    //     }
-        
-    // }
+    // move_pair1(field[0][0],0,0);
+    // print_field();
+    // move_pair1(field[0][0],0,0);
+    // print_field();
+    // move_pair1(field[0][0],0,0);
+    // print_field();
+    // move_pair1(field[0][0],0,0);
+    // move_pair1(field[0][10],10,0);
+    for (int x = 0; x < field_size; x += 2)
+    {
+        while (field[0][x] != field[0][x + 1])
+        {
+            move_pair1(field[0][x], x, 0);
+            print_field();
+        }
+    }
+    print_field();
+    for(int x = 0; x < field_size; x += 2){
+        while(field[1][x] != field[1][x+1]){
+            move_pair1(field[1][x],x,1);
+            print_field();
+        }
+    }
+    //move_pair1(field[1][0], 0, 1);
     print_field();
 
     export_answer();
